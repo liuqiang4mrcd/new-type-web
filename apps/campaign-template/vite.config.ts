d4abdece43239b2788f28219668340859b582898
liuqiang4mrcd <liuqiang@hinterstellar.com>
2026-06-30 16:36:26 +0800

    fix campaign template vite alias

--- apps/campaign-template/vite.config.ts
+++ apps/campaign-template/vite.config.ts
@@ -1,3 +1,11 @@
+import { mergeConfig } from 'vite';
 import { defineCampaignConfig } from '@new-type/config/vite/campaign-base';
+import { fileURLToPath, URL } from 'node:url';
 
-export default defineCampaignConfig({ port: 5173 });
+export default mergeConfig(defineCampaignConfig({ port: 5173 }), {
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
